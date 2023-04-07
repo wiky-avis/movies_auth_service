c74090efd8818c074066217e0d10a9453afe0e3a
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-07 19:23:09 +0300

    изменила docker-compose, добавила миграции

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
+from flask import Flask
+from flask_restx import Api
 from gevent import monkey
 
-from src import create_app
-
+from src import init_db
+from src.api.technical.ping import api as ping_api
 
 monkey.patch_all()
 
-app = create_app()
+app = Flask(__name__)
+
+init_db(app)
+
+api = Api(
+    app=app,
+    title="API Auth",
+    description="API для авторизации пользователей",
+    doc="/api/swagger/",
+    version="1.0.0",
+)
+api.add_namespace(ping_api)
+
 
 if __name__ == "__main__":
     app.run()
