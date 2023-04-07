5807547a9cf0c3b008c678c264d80674fa7a7933
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-07 23:30:28 +0300

    модели

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+
 convention = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -5,6 +5,26 @@ from sqlalchemy.dialects.postgresql import UUID
 from src import database as db
 
 
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name = db.Column(db.String(), unique=True, nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    users = db.relationship(
+        "User", secondary="default", back_populates="roles"
+    )
+
+    def __repr__(self):
+        return f"<Role: {self.name}>"
+
+
 class User(db.Model):
     __tablename__ = "users"
 
@@ -15,8 +35,47 @@ class User(db.Model):
         unique=True,
         nullable=False,
     )
-    login = db.Column(db.String, unique=True, nullable=False)
+    login = db.Column(db.String, unique=True, nullable=True)
+    email = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    login_history = db.relationship("LoginHistory", backref="user")
 
     def __repr__(self):
         return f"<User {self.login}>"
+
+
+class LoginHistory(db.Model):
+    __tablename__ = "login_history"
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False,
+    )
+    user_id = db.Column(
+        UUID(as_uuid=True), db.ForeignKey(User.id, ondelete="CASCADE")
+    )
+    device_type = db.Column(db.String(), primary_key=True)
+    login_dt = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<LoginHistory: (User: {self.user_id}, {self.datetime}>"
+
+
+class UserRole(db.Model):
+    __tablename__ = "user_role"
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    user_id = db.Column(
+        UUID(as_uuid=True), db.ForeignKey(User.id, ondelete="CASCADE")
+    )
+    role_id = db.Column(
+        UUID(as_uuid=True), db.ForeignKey(Role.id, ondelete="CASCADE")
+    )
