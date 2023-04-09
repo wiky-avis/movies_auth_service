7e40eb5f84875ec53777f433603f99a1be126b91
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-09 17:00:21 +0300

    изменила метод create_user

--- src/repositories/auth_repository.py
+++ src/repositories/auth_repository.py
@@ -10,9 +10,7 @@ class AuthRepository:
     def find_by_email(self, email):
         return self.db.session.query(User).filter_by(email=email).first()
 
-    def create_user(self, email: str, password: str):
-        new_user = User()
-        new_user.email = email
-        new_user.password = password
+    def create_user(self, **kwargs):
+        new_user = User(**kwargs)
         self.db.session.add(new_user)
         self.db.session.commit()
