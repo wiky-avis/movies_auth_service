ed9e644edf49abb265aea9427ccd6047e0e10677
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-09 08:28:58 +0300

    рефакт

--- src/api/v1/auth/checking_mail.py
+++ src/api/v1/auth/checking_mail.py
@@ -1,8 +1,7 @@
 from http import HTTPStatus
 
-from flask_restx import Namespace, Resource, reqparse
+from flask_restx import Namespace, Resource, fields, reqparse
 
-from src.api.v1.auth.dto import checking_mail_model
 from src.db.db_factory import db
 from src.repositories.auth_repository import AuthRepository
 from src.services.auth_service import AuthService
@@ -13,13 +12,27 @@ parser = reqparse.RequestParser()
 parser.add_argument("email", type=str)
 
 
+checking_mail_model_response = api.model(
+    "CheckingMailModelResponse",
+    {
+        "id": fields.String(required=True),
+        "email": fields.String(required=True),
+    },
+)
+
+
 @api.route("/checking_mail")
-@api.param("email", "Почта пользователя")
 class CheckingMail(Resource):
-    @api.response(
-        int(HTTPStatus.OK), "User already exist.", checking_mail_model
+    @api.doc(
+        responses={
+            int(HTTPStatus.OK): (
+                "User already exist.",
+                checking_mail_model_response,
+            ),
+            int(HTTPStatus.NOT_FOUND): "User does not exist.",
+        }
     )
-    @api.response(int(HTTPStatus.NOT_FOUND), "User does not exist.")
+    @api.param("email", "Почта пользователя")
     def get(self):
         args = parser.parse_args()
         email = args["email"]
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,17 +1,8 @@
 from http import HTTPStatus
 
-from sqlalchemy import inspect
-from werkzeug.security import check_password_hash
-
-from src.api.v1.auth.models.user import UserResponse
+from src.api.v1.models.response import UserResponse
 from src.common.response import BaseResponse
 from src.repositories.auth_repository import AuthRepository
-
-
-def object_as_dict(obj):
-    return {
-        c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs
-    }
 
 
 class AuthService:
@@ -29,9 +20,3 @@ class AuthService:
             )
         result = UserResponse(id=str(user.id), email=user.email).dict()
         return BaseResponse(success=True, result=result).dict(), HTTPStatus.OK
-
-    @staticmethod
-    def check_password(password_hash, password):
-        if not password:
-            return False
-        return check_password_hash(password_hash, password)
