44741c33a25f0b232978b484b1f072f07e04695d
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-13 23:38:59 +0300

    добавила тесты

--- tests/functional/src/api/v1/test_get_list_history.py
+++ tests/functional/src/api/v1/test_get_list_history.py
@@ -4,7 +4,7 @@ import pytest
 
 from src.config import TEST_PUBLIC_KEY
 from src.repositories.auth_repository import AuthRepository
-from tests.functional.vars.auth import BAD_TOKEN, EXP_TOKEN, NO_TOKEN, sign_jwt
+from tests.functional.vars.auth import sign_jwt
 from tests.functional.vars.login_history import USER_LOGIN_HISTORY
 from tests.functional.vars.tables import CLEAN_TABLES
 
@@ -77,29 +77,3 @@ def test_get_list_history_pagination(
     assert pagination["next_page"] == next_page
     assert pagination["has_next"] == has_next
     assert pagination["has_prev"] == has_prev
-
-
-@pytest.mark.usefixtures("clean_table")
-@pytest.mark.parametrize("clean_table", [CLEAN_TABLES], indirect=True)
-@pytest.mark.parametrize(
-    "token_header",
-    [NO_TOKEN, BAD_TOKEN, EXP_TOKEN],
-)
-def test_get_list_history_undefined_user(
-    test_db,
-    test_client,
-    setup_url,
-    create_list_user_login_history,
-    monkeypatch,
-    token_header,
-):
-    monkeypatch.setattr("src.config.Config.JWT_PUBLIC_KEY", TEST_PUBLIC_KEY)
-    headers = {"X-Auth-Token": token_header}
-
-    res = test_client.get(f"/api/v1/users/login_history", headers=headers)
-    assert res.status_code == HTTPStatus.UNAUTHORIZED
-    assert res.json == {
-        "success": False,
-        "error": {"msg": "UndefinedUser."},
-        "result": None,
-    }
