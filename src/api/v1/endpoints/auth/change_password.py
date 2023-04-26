d1f9696f02fffcfcfc7f6589e06a60707c402213
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-04-26 22:06:40 +0300

    рефакт

--- src/api/v1/endpoints/auth/change_password.py
+++ src/api/v1/endpoints/auth/change_password.py
@@ -2,10 +2,7 @@ import logging
 from http import HTTPStatus
 
 from flask import request
-from flask_jwt_extended import decode_token
-from flask_jwt_extended.exceptions import JWTDecodeError
 from flask_restx import Namespace, Resource
-from jwt import DecodeError
 
 from src.api.v1.dto.base import ErrorModel, ErrorModelResponse
 from src.api.v1.dto.change_data import (
@@ -13,6 +10,7 @@ from src.api.v1.dto.change_data import (
     UserChangePasswordResponse,
 )
 from src.common.collections import get_in
+from src.common.decode_auth_token import get_decoded_data
 from src.common.response import BaseResponse
 from src.db import db_models
 from src.repositories.auth_repository import AuthRepository
@@ -56,10 +54,7 @@ class UserChangeData(Resource):
     @api.expect(InputUserChangePassword)
     def patch(self):
         access_token = request.cookies.get("access_token_cookie")
-        try:
-            decoded_token = decode_token(access_token)
-        except (DecodeError, JWTDecodeError):
-            decoded_token = None
+        decoded_token = get_decoded_data(access_token)
         auth_user_id = get_in(decoded_token, "sub", "user_id")
         if not auth_user_id:
             logger.warning("Failed to get auth_user_id.")
--- src/common/decode_auth_token.py
+++ src/common/decode_auth_token.py
@@ -1,11 +1,10 @@
 import logging
-import os
 from typing import Optional
 
-import jwt
 from dotenv import load_dotenv
-
-from src.config import TEST_PUBLIC_KEY
+from flask_jwt_extended import decode_token
+from flask_jwt_extended.exceptions import JWTDecodeError
+from jwt import DecodeError, ExpiredSignatureError
 
 
 load_dotenv()
@@ -14,21 +13,13 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 
 
-JWT_SECRET = os.getenv("JWT_SECRET", default=TEST_PUBLIC_KEY)
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", default="RS256")
-
-
-def get_decoded_data(token_header: Optional[str]) -> Optional[dict]:
-    if not token_header:
+def get_decoded_data(access_token: Optional[str]) -> Optional[dict]:
+    if not access_token:
         return None
 
     try:
-        decoded_token = jwt.decode(
-            token_header,
-            JWT_SECRET,
-            algorithms=[JWT_ALGORITHM],
-        )
-    except (jwt.ExpiredSignatureError, jwt.DecodeError):
+        decoded_token = decode_token(access_token)
+    except (DecodeError, JWTDecodeError, ExpiredSignatureError):
         logger.error("Invalid token or expired signature.", exc_info=True)
-        return None
+        return
     return decoded_token
